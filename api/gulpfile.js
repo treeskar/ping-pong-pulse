f787571e37e08322ab6966d7961b3c4abc52532a
treeskar <treeskar@gmail.com>
2018-05-21 23:31:01 +0300

    [*] API: :bug: fix nodemon file watching configuration

--- api/gulpfile.js
+++ api/gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('compile', ['lint'], function() {
 gulp.task('watch', ['compile'], function() {
   return nodemon({
     script: 'dist/index.js',
-    watch: 'src',
+    ext: 'ts',
     tasks: ['compile']
   });
 });
